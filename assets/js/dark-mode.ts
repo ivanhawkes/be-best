3c446e3894a224d5b675028713168007787af1d3
Ivan Hawkes <ivan.hawkes@gmail.com>
2026-05-02 19:26:51 +1000

    Dark mode change based on reading instructions. Still too little contrast on the prose sections. Dark circle is back around the menu icons.

--- assets/js/dark-mode.ts
+++ assets/js/dark-mode.ts
@@ -1,13 +1,20 @@
+// Enable dark mode by adding the 'dark' class to the body 
+// and updating localStorage.
 const enableDarkMode = () => {
   document.body.classList.add('dark')
+  document.body.classList.add('dark:prose-invert')
   localStorage.setItem('darkMode', 'enabled')
 }
 
+// Disable dark mode by removing the 'dark' class from the body
+// and updating localStorage.
 const disableDarkMode = () => {
   document.body.classList.remove('dark')
+  document.body.classList.remove('dark:prose-invert')
   localStorage.setItem('darkMode', 'disabled')
 }
 
+// Initialize dark mode based on user preference.
 export const initDarkMode = () => {
   const themeSwitch = document.getElementById('theme-switch')
 
@@ -25,14 +32,15 @@ export const initDarkMode = () => {
       },
       false
     )
-    
+
     const darkMode = localStorage.getItem('darkMode') || 'disabled'
     if (darkMode !== 'enabled') {
-      document.body.classList.remove('dark')
+      disableDarkMode()
     } else {
-      document.body.classList.add('dark')
+      enableDarkMode()
     }
   }
 }
 
+// Initialize dark mode on page load.
 initDarkMode()
